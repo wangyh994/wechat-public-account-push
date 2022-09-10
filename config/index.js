caaa5bd34e4cc86ac47202658d5e3314e7d0ffef
wangyh994 <58500130+wangyh994@users.noreply.github.com>
2022-09-10 15:21:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "甜甜",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ojZKR6g8oD9QXp-CkgyOJ5SEAgSM",
+   *    id: "ojZKR6okTncR3BCN47r8iUxib-w0",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "OPCjiVNmVtqJC_O_0RVstuVrymLfuoRePCCrcF3xOQ4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -33,7 +33,7 @@ export const config = {
       // 想要发送的人的名字
       name: "甜甜",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ojZKR6g8oD9QXp-CkgyOJ5SEAgSM",
+      id: "ojZKR6okTncR3BCN47r8iUxib-w0",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "OPCjiVNmVtqJC_O_0RVstuVrymLfuoRePCCrcF3xOQ4",
       // 所在省份
