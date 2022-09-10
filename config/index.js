96bdf370672ab4b07d173e40259c644f07e63092
wangyh994 <58500130+wangyh994@users.noreply.github.com>
2022-09-10 15:10:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "ojZKR6g8oD9QXp-CkgyOJ5SEAgSM",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "uwi6rL9Cq_PNOeSE4L43eebcIh-7Gt8w7pvjXB_xkKo",
+   *    useTemplateId: "OPCjiVNmVtqJC_O_0RVstuVrymLfuoRePCCrcF3xOQ4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://www.corgiorgy.com/"
    *   },
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ojZKR6g8oD9QXp-CkgyOJ5SEAgSM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "uwi6rL9Cq_PNOeSE4L43eebcIh-7Gt8w7pvjXB_xkKo",
+      useTemplateId: "OPCjiVNmVtqJC_O_0RVstuVrymLfuoRePCCrcF3xOQ4",
       // 所在省份
       province: "河北",
       // 所在城市
